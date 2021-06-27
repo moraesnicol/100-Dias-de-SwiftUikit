7f0dd7fbd45483bfcbae15313a79cd8f3c93336b
Gabriel <moraesnicol@gmail.com>
2021-06-26 22:02:18 -0300

    day 6 init

--- Day 5/Functions.playground/Contents.swift
+++ Day 5/Functions.playground/Contents.swift
@@ -78,3 +78,35 @@ func square3(x: Int...) {
     }
 }
 square3(x: 1, 2, 3, 4, 5)
+
+
+// Sometimes functions fail because they have bad input, or because something went wrong internally. Swift lets us throw errors from functions by marking them as throws before their return type, then using the throw keyword when something goes wrong.
+
+// First we need to define an enum that describes the errors we can throw. These must always be based on Swift’s existing Error type. We’re going to write a function that checks whether a password is good, so we’ll throw an error if the user tries an obvious password:
+
+enum PasswordError: Error {
+    case obvious
+}
+
+func checkPassword(_ password: String) throws -> Bool {
+    if password == "password" {
+        throw PasswordError.obvious
+    }
+
+    return true
+}
+do {
+    try checkPassword("password")
+    print("That password is good!")
+} catch {
+    print("You can't use that password.")
+}
+
+//
+
+func doubleInPlace(number: inout Int) {
+    number *= 2
+}
+var myNum = 10
+var x = doubleInPlace(number: &myNum)
+print(x)
